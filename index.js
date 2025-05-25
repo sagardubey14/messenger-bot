5557ad9feb35a7d19851afbd4dbccfe691dde3f4
sagardubey14 <sagardubey353@gmail.com>
2025-05-25 13:55:02 +0530

    Integrate Ngrok tunneling for public webhook URL

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const ngrok = require('@ngrok/ngrok');
+const webhookRoutes = require('./routes/webhookRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.use('/', webhookRoutes);
+
+
+app.listen(PORT, async () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+  try {
+    const listener = await ngrok.connect({
+      addr: PORT,
+      authtoken_from_env: true,
+      domain: process.env.NGROK_DOMAIN,
+    });
+    console.log(`Ngrok tunnel established at: ${listener.url()}`);
+  } catch (err) {
+    console.error('Failed to start ngrok:', err);
+  }
 });
